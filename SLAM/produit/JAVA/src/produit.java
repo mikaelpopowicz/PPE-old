2bed6479ba9cc749b6e6995dd094708f62cb30bd
mik0_91 <mikael.popowicz@gmail.com>
2013-10-15 16:31:19 +0200

    Cours

--- SLAM/produit/JAVA/src/produit.java
+++ SLAM/produit/JAVA/src/produit.java
@@ -4,27 +4,70 @@ public class Produit {
 	private String designation;
 	private float prix;
 
+	
+	/*
+	 *  @@ Constructeur
+	 */
+	
 	public Produit(int ref, int qte, float prix, String des) {
+		this.setRef(ref);
+		this.setQte(qte);
+		this.setPrix(prix);
+		this.setDes(des);
+	}
+	
+	
+	/*
+	 *  @@ Getters
+	 */
+	
+	public int ref() { return this.ref; }
+	public int qte() { return this.qte; }
+	public String des() { return this.designation; }
+	public float prix() { return this.prix; }
+	
+	
+	/*
+	 *  @@ Setters
+	 */
+	
+	public void setRef(int ref) {
 		this.ref = ref;
+	}
+	
+	public void setQte(int qte) {
 		this.qte = qte;
-		this.prix = prix;
+	}
+	
+	public void setDes(String des) {
 		this.designation = des;
 	}
 	
+	public void setPrix(Float prix) {
+		this.prix = prix;
+	}
+	
+	
+	
+	/*
+	 *  @@ Fonctions supplémentaires
+	 */
+	
 	public String afficher() {
 		String aff = "";
-		aff += "Référence: "+this.ref+"\n";
-		aff += "Quantité: "+this.qte+"\n";
+		aff += "Référence: "+this.ref()+"\n";
+		aff += "Quantité: "+this.qte()+"\n";
+		aff += "Description: "+this.des()+"\n";
+		aff += "Prix: "+this.prix()+"\n";
 		return aff;
 	}
 	
 	public float total_prix() {
-		float total = this.prix*this.qte;
+		float total = this.prix()*this.qte();
 		return total;
 	}
 	
-	
-	
+
 	public static void main(String[] args) {
 		Produit test = new Produit(1, 15, 575, "Un produit de fou");
 		System.out.println(test.afficher());
