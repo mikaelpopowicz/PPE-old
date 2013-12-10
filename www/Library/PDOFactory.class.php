c6eae69ae05c42ac8d92db4a948d808bbad4f7cb
mik0_91 <mikael.popowicz@gmail.com>
2013-12-10 13:56:20 +0100

    Cours

--- www/Library/PDOFactory.class.php
+++ www/Library/PDOFactory.class.php
@@ -7,7 +7,7 @@ class PDOFactory
 	{
 		$db = new \PDO('mysql:host=localhost;dbname=coaching', 'coaching', 'motdepasse');
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-     
+		//echo "COUCOU";
 		return $db;
 	}
 }
