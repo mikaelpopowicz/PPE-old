447e0d7f6669c7629ca4b7224555a7108982496f
mik0_91 <mikael.popowicz@gmail.com>
2013-12-10 13:48:49 +0100

    Correction

--- www/Library/PDOFactory.class.php
+++ www/Library/PDOFactory.class.php
@@ -5,11 +5,7 @@ class PDOFactory
 {
 	public static function getMysqlConnexion()
 	{
-<<<<<<< HEAD
-		$db = new \PDO('mysql:host=localhost;dbname=coaching', 'root', 'root');
-=======
 		$db = new \PDO('mysql:host=localhost;dbname=coaching', 'coaching', 'motdepasse');
->>>>>>> 0d740969407ba16e6e5df105589b6b0e75cd2c49
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
      
 		return $db;
