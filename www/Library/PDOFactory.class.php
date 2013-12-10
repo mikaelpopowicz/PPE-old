ed3511cd59168e6265c5552b93deb644ac328ce5
pauluss75 <paul.soubrane@gmail.com>
2013-12-10 11:31:49 +0100

    dbname = coaching au lieu de cinéma

--- www/Library/PDOFactory.class.php
+++ www/Library/PDOFactory.class.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
 	public static function getMysqlConnexion()
 	{
-		$db = new \PDO('mysql:host=localhost;dbname=cinema', 'root', 'root');
+		$db = new \PDO('mysql:host=localhost;dbname=coaching', 'root', 'root');
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
      
 		return $db;
