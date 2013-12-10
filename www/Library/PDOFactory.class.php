b2f1e6e3e1ce198c991723392627cf8b9e0ca1a7
mik0_91 <mikael.popowicz@gmail.com>
2013-12-10 13:19:01 +0100

    Cours

--- www/Library/PDOFactory.class.php
+++ www/Library/PDOFactory.class.php
@@ -5,11 +5,7 @@ class PDOFactory
 {
 	public static function getMysqlConnexion()
 	{
-<<<<<<< HEAD
 		$db = new \PDO('mysql:host=localhost;dbname=coaching', 'root', 'toor');
-=======
-		$db = new \PDO('mysql:host=localhost;dbname=coaching', 'root', 'root');
->>>>>>> 9ec8f32198a5ed7ad8c931d0ca56652edc82cfb4
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
      
 		return $db;
